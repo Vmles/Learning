281e41c1d7cdeee91d6ad0142a2d85d8d1f70e77
VictorHMS <39633965+VictorHMS@users.noreply.github.com>
2020-10-30 20:17:10 -0300

    Create In_Out.cpp

---
+++ In_Out.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main(){
+  printf("Hello Wolrd\n");
+  std::cout << "Hello Wolrd" << std::endl;
+  
+  int a, b;
+  
+  scanf("%d",&a);
+  std::cin >> b;
+  
+  printf("Li o valor: %d de a\n", a);
+  std::cout << "Li o valor: " << b << " de b" << std::endl;
+  return 0;
+}
